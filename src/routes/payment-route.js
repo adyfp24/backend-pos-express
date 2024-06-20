1e7d7f91fe4d716f870968e3147b91d06a6accd9
adyfp24 <adyfp24@gmail.com>
2024-06-20 19:20:44 +0700

    feat: setup for payment routes

---
+++ src/routes/payment-route.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const paymentController = require('../controllers/payment-controller');
+const { validate } = require('../middlewares/validator');
+const paymentValidation = require('../validations/payment-validation');
+
+router.post('/payment', );
+router.get('/payment/struct', );
